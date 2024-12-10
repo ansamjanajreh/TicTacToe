1873a8633b98521fe36cc90ede6795a8f8294a06
ansamjanajreh <ansam.janajreh02@gmail.com>
2024-12-10 14:25:42 +0200

    Delete backend directory

--- backend/logger.js
+++
@@ -1,8 +0,0 @@
-const winston = require("winston");
-let { createLogger } = require("winston");
-
-let logger = createLogger({
-  transports: [new winston.transports.File({ filename: "ServerLogs.log" })],
-});
-
-module.exports = logger;
